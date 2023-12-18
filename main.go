7cf0a69f544244c3e5fbd91eebd2e22e4d8bf831
Prabal2308 <prabalsharma2308@gmail.com>
2023-12-18 08:39:55 +0530

    adding handlers to handle various CRUD operations

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	"github.com/Prabal2308/Zopsmart_Mini_Project/datastore"
+	"github.com/Prabal2308/Zopsmart_Mini_Project/handler"
 
-func main() {
-    app := gofr.New()
+	"gofr.dev/pkg/gofr"
+)
 
-    app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
-        return "Hello World!", nil
-    })
-
-    app.Start()
-}
+func main() {  
+    app := gofr.New()  
+    
+    s := datastore.New()  
+    h := handler.New(s)  
+    
+    app.GET("/students/{id}", h.GetByID)  
+    app.POST("/students", h.Create)  
+    app.PUT("/students/{id}", h.Update)  
+    app.DELETE("/students/{id}", h.Delete)  
+    
+    // starting the server on a custom port  
+    app.Server.HTTP.Port = 9092  
+    app.Start()  
+  }
