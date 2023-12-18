04f9cb40c3b852e6287abc92095c6a63728a8aff
Prabal2308 <prabalsharma2308@gmail.com>
2023-12-18 08:23:51 +0530

    creating Student interface for interacting with Student database

---
+++ datastore/interface.go
@@ -0,0 +1,17 @@
+package datastore  
+  
+import (  
+  "github.com/Prabal2308/Zopsmart_Mini_Project/model"  
+  
+ "gofr.dev/pkg/gofr")  
+  
+ type Student interface {
+	// GetByID retrieves a student record based on its ID.
+	GetByID(ctx *gofr.Context, id string) (*model.Student, error)
+	// Create inserts a new student record into the database.
+	Create(ctx *gofr.Context, model *model.Student) (*model.Student, error)
+	// Update updates an existing student record with the provided information.
+	Update(ctx *gofr.Context, model *model.Student) (*model.Student, error)
+	// Delete removes a student record from the database based on its ID.
+	Delete(ctx *gofr.Context, id int) error
+}
